455bd8385ebde81c1b46d04cb057dbcd7ac17578
egorka001 <volokitine@yandex.ru>
2022-12-04 16:32:32 +0300

    initial commit

--- app/interface/config_read.py
+++
@@ -1,2 +0,0 @@
-def config_read():
-    return 'base.db'
--- script.py
+++
@@ -1,6 +0,0 @@
-import sys
-sys.path.append('./app/database/')
-sys.path.append('./app/interface/')
-from init_interface import *
-
-db_init()
