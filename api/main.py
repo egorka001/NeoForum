cbe44126f0c2ff7065bc134e13de5527e9c19f9e
egorka001 <volokitine@yandex.ru>
2022-12-04 21:57:14 +0300

    api stress testing

--- api/main.py
+++ api/main.py
@@ -4,41 +4,52 @@ themes_sample = """{"themes" : ["sport", "motosport", "programming", "offtop"]}"
 
 thread_sample = """{"creator_name" : "Максим", "thread_titile":"Лечу в сочи", "posts" : [{"post_id" : "0", "date" : "01.12.2022", "time" : "13:41", "post_creator" : "Максим", "post_text" : "Что лучше с собой взять на отдых?", "post_pic" : "base64 pic code"},{"post_id" : "1", "date" : "01.12.2022", "time" : "13:42", "post_creator" : "Егор", "post_text" : "Средство от загара", "post_pic" : "base64 pic code"},{"post_id" : "2", "date" : "01.12.2022", "time" : "13:43", "post_creator" : "Антон", "post_text" : "Лучше ничего с собой не брать, так на легке поехать", "post_pic" : ""},{"post_id" : "3", "date" : "01.12.2022", "time" : "13:44", "post_creator" : "Лена", "post_text" : "Может у меня всё таки купят гараж?", "post_pic" : "base64 pic code"}]}"""
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, send_from_directory 
+from flask import jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/favicon.ico', methods=['GET'])
+def favicon():
+    if request.method == 'GET':
+        return send_from_directory('static', 'favicon.ico') 
+
+@app.route('/', methods=['GET'])
 def send_base():
     """send base index.html page"""
-    return render_template('index.html')
+    if request.method == 'GET':
+        return render_template('index.html')
 
-@app.route('/api/')
+@app.route('/api/', methods=['GET'])
 def send_api():
     """send info page about api, api.html"""
-    return render_template('api.html')
+    if request.method == 'GET':
+        return render_template('api.html')
 
-@app.route('/api/themes/')
+@app.route('/api/themes/', methods=['GET'])
 def send_themes_api():
     """send all themes from forum"""
-    return themes_sample 
+    if request.method == 'GET':
+        return jsonify(themes_sample)
 
-@app.route('/api/threads_sport')
-@app.route('/api/threads_motosport')
-@app.route('/api/threads_programming')
-@app.route('/api/threads_offtop')
+@app.route('/api/threads_sport', methods=['GET'])
+@app.route('/api/threads_motosport', methods=['GET'])
+@app.route('/api/threads_programming', methods=['GET'])
+@app.route('/api/threads_offtop', methods=['GET'])
 def send_threads_api():
     """send threads dct from forum"""
-    return threads_sample
+    if request.method == 'GET':
+        return jsonify(threads_sample)
 
-@app.route('/api/thread_1')
-@app.route('/api/thread_2')
-@app.route('/api/thread_3')
-@app.route('/api/thread_4')
-@app.route('/api/thread_5')
+@app.route('/api/thread_1', methods=['GET'])
+@app.route('/api/thread_2', methods=['GET'])
+@app.route('/api/thread_3', methods=['GET'])
+@app.route('/api/thread_4', methods=['GET'])
+@app.route('/api/thread_5', methods=['GET'])
 def send_thread_api():
     """send current thread"""
-    return thread_sample 
+    if request.method == 'GET':
+        return jsonify(thread_sample) 
 
 if __name__ == '__main__':
     app.run()
